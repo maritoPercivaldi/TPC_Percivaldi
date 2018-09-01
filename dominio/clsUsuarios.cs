a2789b24083113e932a2328e3f95cb1ca2e846e4
Mario Percivaldi <percivaldi.mario@gmail.com>
2018-09-01 00:25:53 -0300

    probando conectar usando como corresponde.

--- dominio/clsUsuarios.cs
+++ dominio/clsUsuarios.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace dominio
 {
-    class clsUsuarios
+    public class clsUsuarios
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
---
+++ negocio/UsuarioBusiness.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+using System.Data.SqlClient;
+
+
+namespace negocio
+{
+    public class UsuarioBusiness
+    {
+        public void listar()
+        {
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+            clsUsuarios usuarios = new IList<usuarios>();
+
+
+            conexion.ConnectionString = @"Data Source=DESKTOP-5UAJG1S\SQLEXPRESS;Initial Catalog= ksrPanel ;Integrated Security=SSPI";
+            comando.CommandType = System.Data.CommandType.Text;
+            comando = "Select * from USUARIOS";
+            conexion.Open();
+            while(lector.Read())
+            {
+
+            }
+            
+
+
+
+        }
+    }
+}
